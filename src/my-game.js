b669787aa8118b3f98e62f8d9c3b52340f0afcfb
gudvinga <gudvinga@gmail.com>
2017-09-24 12:02:23 +0300

    Work in process

--- src/my-game.js
+++ src/my-game.js
@@ -12,19 +12,28 @@ class GuessingGame {
 
     guess() {
         this.number = Math.floor(Math.random()*(this.maxRange - this.minRage) + this.minRage);
+        return this.number;
     }
 
     lower() {
-        if (this.number > this.value) {
-            this.maxRange = this.number;
+        if (this.guess() > this.value) {
+            this.maxRange = this.guess();
+            this.greater();
         }
+        if (this.number == this.value) return this.number;
     }
 
     greater() {
         if (this.number < this.value) {
             this.minRange = this.number;
+            this.lower();
         }
+        if (this.number == this.value) return this.number;
     }
 }
 
-module.exports = GuessingGame;
+var game = new GuessingGame(15);
+game.setRange(18,200);
+game.guess();
+
+console.log(game.lower());
